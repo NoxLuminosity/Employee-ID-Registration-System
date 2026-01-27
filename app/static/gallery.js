f78d2a8948920b9ae5d0f0305054a1f368a55f6f
unknown <manuelmiguel0726@gmail.com>
2026-01-27 13:21:29 +0800

    Finals Checks

--- app/static/gallery.js
+++ app/static/gallery.js
@@ -766,7 +766,7 @@ function generateIDCardBackHtml(emp) {
               <path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path>
             </svg>
           </div>
-          <span class="id-back-website-url">${escapeHtml(backDynamicUrl)}</span>
+          <span class="id-back-website-url">${escapeHtml(dynamicUrl)}</span>
         </div>
       </div>
     </div>
